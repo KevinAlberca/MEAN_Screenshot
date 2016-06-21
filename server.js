9470a886426212a9d69279ba11b653b578684a52
Kevin Alberca <kevin.alberca@supinternet.fr>
2016-06-21 16:04:19 +0200

    Change http to express

--- server.js
+++ server.js
@@ -3,29 +3,34 @@
  */
 
 var received_website = [],
-    app = require('http').createServer(handler),
-    io = require('socket.io')(app),
-    fs = require('fs');
+    express = require('express'),
+    app = express(),
+    server = require('http').Server(app),
+    io = require('socket.io')(server),
+    webshot = require('webshot');
 
-app.listen(8080);
 
-function handler (req, res) {
-    fs.readFile(__dirname + '/public/index.html',
-        function (err, data) {
-            if (err) {
-                res.writeHead(500);
-                return res.end('Error loading index.html');
-            }
 
-            res.writeHead(200);
-            res.end(data);
-        });
-}
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+app.use("/", express.static("./"));
+app.use("/public/img", express.static("./public/img"));
+
+server.listen(8080);
 
 io.on('connection', function (socket) {
-    socket.emit('news', { hello: 'world' }); // Envoi un objet nomme
-    socket.on('new website', function (data) {
-        received_website.push(data);
-        console.log(received_website);
+    socket.emit('website_list', received_website);
+    socket.on('new_website', function (data) {
+        data.screenshot = "Empty screenshot";
+        var img_name = data.url.substr(7).replace(".", "_");
+
+        webshot(data.url, __dirname+'/public/img/'+img_name+'.png', function(err) {
+          // screenshot now saved to google.png
+          data.screenshot = '/public/img/'+img_name+'.png';
+          received_website.push(data);
+          socket.emit('website_list', received_website);
+        });
     });
 });
